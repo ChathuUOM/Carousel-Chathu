7ebb4141f319a485d0ade33cdfc80c2ac8e31203
Chathurika Wijekoon <chathurikaw@embla.asia>
2023-09-14 00:58:46 +0530

    feat: :memo: Card content is updated

--- src/app/components/card-carousel/card-carousel.component.ts
+++ src/app/components/card-carousel/card-carousel.component.ts
@@ -26,12 +26,14 @@ export class CardCarouselComponent implements OnInit {
       {
         id: 1,
         title: 'Apollo 11',
-        description: 'A Walk on the Moon',
+        description:
+          'A Walk on the Moon, Apollo 11 was the first mission to actually land men on the moon.',
       },
       {
         id: 2,
         title: 'Freedom 7',
-        description: 'The First American in Space',
+        description:
+          'The First American in Space; The first American astronaut to orbit the Earth was Alan Shepard, and he left Earth on May 5, 1961. He was not the first human in space; a Soviet astronaut named Yuri Gagarin has that distinction.',
       },
       {
         id: 3,
@@ -41,12 +43,14 @@ export class CardCarouselComponent implements OnInit {
       {
         id: 4,
         title: 'Apollo 8',
-        description: 'A Lunar Christmas Eve',
+        description:
+          'A Lunar Christmas Eve; Astronauts Frank Borman, James A. Lovell, and William Anders — who would been preparing themselves for a very different kind of mission — were assigned to this endeavor. ',
       },
       {
         id: 5,
         title: 'The Space Shuttle',
-        description: 'A Reusable Spacecraft',
+        description:
+          'A Reusable Spacecraft: It took nine years from the start of the project to the first flight. In 1981, the Space Shuttle Columbia lifted off and its maiden mission was successful.',
       },
       {
         id: 6,
@@ -61,7 +65,8 @@ export class CardCarouselComponent implements OnInit {
       {
         id: 8,
         title: 'Chandra X-ray Observatory',
-        description: 'The High-energy Universe',
+        description:
+          'The High-energy Universe: X-ray telescopes are different than the more commonplace optical telescopes we are all familiar with.',
       },
       {
         id: 9,
